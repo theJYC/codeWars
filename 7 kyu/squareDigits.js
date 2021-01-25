ab8d11e79e23228b9a28e1191e69d2dfcbba34be
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2021-01-25 14:28:41 -0500

    leveled up!

---
+++ 7 kyu/squareDigits.js
@@ -0,0 +1,26 @@
+//squareDigits.js
+
+/*
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 9^2 is 81 and 1^2 is 1.
+
+Note: The function accepts an integer and returns an integer
+*/
+
+
+squareDigits = num => {
+    //create a string variable to be populated & returned
+    str = "";
+    //make integer into an array
+    newNum = num.toString().split("");
+    console.log(newNum)
+    //loop through and square each index
+    for (i = 0; i < num.length; i++) {
+        //square each digits of the array
+        (parseInt(i)**2).toString();
+    }
+    return num;
+}
+
+console.log(squareDigits(9119));
